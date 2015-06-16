535112fcf04774f37c64538784b7cc25c9041e4f
Martijn <martijn@vanderlee.com>
2015-06-16 22:12:36 +0200

    Minor tweaks

---
+++ README.md
@@ -0,0 +1,51 @@
+jQuery.px
+=========
+Version 1.0.2
+
+Copyright (c) 2011-2015 Martijn W. van der Lee
+Licensed under the MIT.
+
+Measure an amount of CSS3 units in pixels, relative to a specified context if available.
+Based on http://www.filamentgroup.com/lab/update_jquery_plugin_for_retaining_scalable_interfaces_with_pixel_to_em_con/
+
+Syntax
+------
+`$(<context>).px(<size>);`
+`$.px(<size>);`
+    
+Arguments
+---------
+context (optional)
+	The DOM tree node in which to measure. Some units like 'em' and '%' are relative to the current font size.
+	If not specified, 'body' will be used for units that require a context.
+size    (optional)
+	A number and CSS3 unit or constant that should be measured.
+	The number can be integer or floating point and may be preceded by a sign (-/+).
+	The unit can be any of the following:
+		px, in, cm, mm, pt, pc                      (no context)
+		em, ex, rem                                 (uses context)
+		vw, vh, vm                                  (no context)
+		vd											(no context, not a CSS3 unit; hypothenuse of vw and vh)
+		thin, thick                                 (border widths)
+		border-thin, border-medium, border-thick    (alternative names for border widths)
+		%                                           (relative to current font-size, uses context)
+		xx-small, x-small, small, medium,
+		 large, x-large, xx-large                   (font-size names, no context)
+		smaller, larger                             (relative font-size names, uses context)                            
+	Both the number and unit/constant are optional; number will default to '1', unit will default to 'em'.
+    
+Return
+------
+pixels 
+	The measured size in pixel units.
+	If the unit is specified but not recognized, an error will be thrown.
+
+Examples
+--------
+`$.px();                // returns the current font-size (1em) set in <body>, measured in pixels.`
+`$.px('thin');          // returns the "thin" border-width, measured in pixels.`
+`$.px('small');         // returns the "small" font-size. measured in pixels.`
+`$.px('2in');           // returns the number of pixels in 2 inches.`
+`$.px('200%');          // returns the number of pixels in a font-size 200% the size of the font in <body>.`
+`$('#here').px('200%'); //  ... same as above, but relative to font-size in the tag with id="here".`
+`$('#here').px('in');   // returns the number of pixels in an inch. The context is ignored.`
---
+++ jquery.px.js
@@ -0,0 +1,122 @@
+/*jslint devel: true, bitwise: true, regexp: true, browser: true, confusion: true, unparam: true, eqeq: true, white: true, nomen: true, plusplus: true, maxerr: 50, indent: 4 */
+/*globals jQuery */
+
+/*! px
+ * Copyright (c) 2011-215 Martijn W. van der Lee
+ * Licensed under the MIT.
+ */
+/* Measure an amount of CSS3 units in pixels, relative to a specified context if available.
+ * Based on http://www.filamentgroup.com/lab/update_jquery_plugin_for_retaining_scalable_interfaces_with_pixel_to_em_con/
+ */
+
+(function($, undefined){
+	$.fn.px = function(size) {
+		var element = $('<div style="display:none;height:10in;border:0;margin:0;padding:0"></div>').appendTo('body'),
+			inch	= element.height() / 10.;
+		element.remove();
+		
+		var win = $(window);
+	
+		function _measureFontSize(name, context) {
+			var element = $('<div style="display:none;font-size:'+name+';border:0;margin:0;padding:0"><div style="display:none;height:1em;border:0;margin:0;padding:0"></div></div>').appendTo(context),
+				width	= $('div', element).height();
+			element.remove();
+			return width;
+		}	
+	
+		function _measureRelativeUnit(unit, context) {
+			var element = $('<div style="display:none;height:10'+unit+';margin:0;padding:0;border:0;"></div>').appendTo(context),
+				px		= element.height() / 10.;
+			element.remove();
+			return px;
+		}
+		
+		function _measureBorderWidth(width) {
+			var element = $('<div style="display:none;border:'+width+' solid transparent;height:0;margin:0;padding:0"></div>').appendTo('body'),
+				width = element.outerHeight() / 2.;
+			element.remove();
+			return width;
+		}		
+	
+		var m		= /^([-+]?\d*\.?\d*)(\D*)$/i.exec(size),
+			length	= (size && m != null && m[1])? m[1] : 1,
+			unit	= (size && m != null && m[2])? m[2] : 'em',
+			px		= 0;
+
+		switch (unit) {
+		// absolute units
+			case 'px':	px = 1;				break;			
+			case 'in':	px = inch;			break;
+			case 'cm':	px = inch / 2.54;	break;
+			case 'mm':	px = inch / 25.4;	break;
+			case 'pt':	px = inch / 72;		break;
+			case 'pc':	px = inch / 6;		break;
+
+		// relative units
+			case 'em':
+			case 'ex':
+			case 'rem':
+				px = _measureRelativeUnit(unit, this.first());
+				break;
+				
+		// viewport dimensions
+			case 'vw':
+				px = win.width() / 100.;
+				break;
+			case 'vh':
+				px = win.height() / 100.;
+				break;
+			case 'vm':
+				px = Math.min(win.width(), win.height()) / 100.;
+				break;
+			case 'vd':	// non-CSS3!
+				var w = win.width();
+				var h = win.height();
+				px = Math.sqrt((w * w) + (h * h)) / 100.;
+				break;
+				
+		// border - plain name
+			case 'thin':
+			case 'thick':
+				px = _measureBorderWidth(unit);
+				break;
+				
+		// border - "border-" prefix
+			case 'border-thin':
+			case 'border-medium':
+			case 'border-thick':
+				px = _measureBorderWidth(unit.substr(7));
+				break;
+				
+		// font-size - percentage
+			case '%':
+				px = _measureRelativeUnit('em', this.first()) / 100.;	// 1/100th of 1em
+				break;
+				
+		// font-size - plain names (no context)
+			case 'xx-small':
+			case 'x-small':
+			case 'small':
+			case 'medium':
+			case 'large':
+			case 'x-large':
+			case 'xx-large':
+		// font-size - plain names (uses context)
+			case 'smaller':
+			case 'larger':
+				px = _measureFontSize(unit, this.first());
+				break;
+				
+		// unknown unit
+			default:	
+				throw 'Unknown unit "'+unit+'"'; 
+				break;
+		}
+		
+		return length * px;
+	};
+	
+	$.px = function(size) {
+		return $('body').px(size);
+	};
+})(jQuery);
